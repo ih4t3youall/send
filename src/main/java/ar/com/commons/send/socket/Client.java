e8a9e0a548cc019b27054ddc6c0b855cbd80c02d
ih4t3youall <ih4t3youall@gmail.com>
2018-11-21 13:09:11 -0300

    se agrega soporte para sockets

---
+++ src/main/java/ar/com/commons/send/socket/Client.java
@@ -0,0 +1,56 @@
+package ar.com.commons.send.socket;
+
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client implements Runnable {
+
+    String filename;
+    String url;
+    int port = 5000;
+
+    public Client(String filename, String url){
+       this.filename = filename;
+       this.url = url;
+    }
+    public Client(String filename, String url,int port){
+        this.filename = filename;
+        this.url = url;
+        this.port = port;
+    }
+
+    @Override
+    public void run(){
+        DataInputStream input;
+        BufferedInputStream bis;
+        BufferedOutputStream bos;
+        int in;
+        byte[] byteArray;
+        //Fichero a transferir
+        //final String filename = "c:\\test.pdf";
+
+        try{
+            final File localFile = new File( filename );
+            Socket client = new Socket(url, port);
+            bis = new BufferedInputStream(new FileInputStream(localFile));
+            bos = new BufferedOutputStream(client.getOutputStream());
+            //Enviamos el nombre del fichero
+            DataOutputStream dos=new DataOutputStream(client.getOutputStream());
+            dos.writeUTF(localFile.getName());
+            //Enviamos el fichero
+            byteArray = new byte[8192];
+            while ((in = bis.read(byteArray)) != -1){
+                bos.write(byteArray,0,in);
+            }
+
+            bis.close();
+            bos.close();
+
+        }catch ( Exception e ) {
+            e.printStackTrace();
+            System.err.println(e);
+        }
+    }
+
+}
---
+++ src/main/java/ar/com/commons/send/socket/Server.java
@@ -0,0 +1,65 @@
+package ar.com.commons.send.socket;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server implements Runnable{
+
+    private String url;
+    private String directory;
+    private int port;
+
+    public Server(String url, String directory){
+        this.url = url;
+        this.directory = directory;
+        this.port = 5000;
+    }
+    public Server(String url, String directory,int port){
+        this.url = url;
+        this.directory = directory;
+        this.port = port;
+    }
+
+    public void run(){
+
+        ServerSocket server;
+        Socket connection;
+
+        DataOutputStream output;
+        BufferedInputStream bis;
+        BufferedOutputStream bos;
+
+        byte[] receivedData;
+        int in;
+        String file;
+
+        try{
+            //Servidor Socket en el puerto 5000
+            server = new ServerSocket( port );
+            System.out.println("iniciando");
+            while ( true ) {
+                //Aceptar conexiones
+                connection = server.accept();
+                //Buffer de 1024 bytes
+                receivedData = new byte[1024];
+                bis = new BufferedInputStream(connection.getInputStream());
+                DataInputStream dis=new DataInputStream(connection.getInputStream());
+                //Recibimos el nombre del fichero
+                file = dis.readUTF();
+                file = file.substring(file.indexOf('\\')+1,file.length());
+                file = directory +"//"+ file;
+                System.out.println("file: " +file.toString());
+                //Para guardar fichero recibido
+                bos = new BufferedOutputStream(new FileOutputStream(file));
+                while ((in = bis.read(receivedData)) != -1){
+                    bos.write(receivedData,0,in);
+                }
+                bos.close();
+                dis.close();
+            }
+        }catch (Exception e ) {
+            System.err.println(e);
+        }
+    }
+}
